6c596cfa349a1979b1905858c4fec238a04a66ec
ajerzykiewicz <161578867+ajerzykiewicz@users.noreply.github.com>
2024-10-13 16:23:08 +0000

    added main

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+liczba_dokumentow = int(input("Ile dokumentów chcesz wprowadzić? "))
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+dokumenty = []
+
+for i in range(liczba_dokumentow):
+    dokument = input(f"Wpisz treść dokumentu {i+1}: ").strip()
+    dokumenty.append(dokument)
+
+liczba_slow = int(input("Ile słów chcesz szukać? "))
+
+
+slowa = []
+
+for i in range(liczba_slow):
+    slowo = input(f"Wpisz słowo {i+1} do wyszukania: ").strip()
+    slowa.append(slowo)
+
+
+wyniki = {}
+
+
+for i, dokument in enumerate(dokumenty):
+    dokument_id = f"Dokument {i+1}"
+    wyniki[dokument_id] = {}
+    
+    for slowo in slowa:
+        liczba_wystapien = dokument.lower().split().count(slowo.lower())
+        wyniki[dokument_id][slowo] = liczba_wystapien
+
+wyniki_listy = [[] for _ in range(liczba_slow)]
+
+
+for i, slowo in enumerate(slowa):
+    dokumenty_wyniki = sorted(range(liczba_dokumentow), key=lambda x: wyniki[f"Dokument {x+1}"][slowo], reverse=True)
+    wyniki_listy[i] = dokumenty_wyniki
+
+for lista in wyniki_listy:
+    print(lista)
